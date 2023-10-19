7c609e0f06faa5261e591d00622e80aff32172c1
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-10-19 15:20:02 +0200

    obstacles done

--- js/game.js
+++ js/game.js
@@ -9,11 +9,18 @@ class Game {
     this.pollito = new Pollito()
 
     // obstaculos
-    this.oneObstacle = new Obstacle()
+    // this.oneObstacle = new Obstacle()
+    this.obstacleArr = []
 
 
     // timer
     this.timer = 0; // 60 veces por segundo
+
+    // ejemplo de agregar elementos con setInterval
+    // setInterval(() => {
+    //   let newObstacle = new Obstacle()
+    //   this.obstacleArr.push(newObstacle)
+    // }, 2000)  
 
   }
 
@@ -22,17 +29,41 @@ class Game {
 
 
   // aparecer y desaparecer de los obstaculos
+  obstacleAppear = () => {
+
+    // determinar, cuando voy a agregar elementos al array
+    if (this.timer % 120 === 0) {
+
+      // cuando el timer sea divisible entre 120 => han pasado 2 segundo
+
+      let randomPosition = Math.random() * -100 // -100 y 0
+
+      let newObstacleTop = new Obstacle("top", randomPosition)
+      this.obstacleArr.push(newObstacleTop)
+
+      let newObstacleBottom = new Obstacle("bottom", randomPosition + 300)
+      this.obstacleArr.push(newObstacleBottom)
+
+    }
+
+  }
   
   
   // gameLoop
   gameLoop = () => {
     // console.log("ejecutando gameLoop")
-    this.timer++
     // console.log(this.timer)
-
+    
     this.pollito.gravityEffect()
-
+    // this.oneObstacle.automaticMovement()
+    this.obstacleArr.forEach((eachObstacle) => {
+      // esto moveria TODOS los tubos del array
+      eachObstacle.automaticMovement()
+    })
+    this.obstacleAppear()
+    
     // recursion
+    this.timer++
     requestAnimationFrame( this.gameLoop )
 
   }
--- js/obstacle.js
+++ js/obstacle.js
@@ -1,16 +1,23 @@
 class Obstacle {
 
-  constructor() {
+  constructor( type, yPosition ) {
+    // type => o "top" o "bottom"
+
     // nodo
     this.node = document.createElement("img")
-    this.node.src = "./images/obstacle_top.png"
+    if (type === "top") {
+      this.node.src = "./images/obstacle_top.png"
+    } else {
+      this.node.src = "./images/obstacle_bottom.png"
+    }
     gameBoxNode.append(this.node)
 
     // dimensiones de obstaculos
     this.w = 60 // weight => ancho
     this.h = 200 // heigth => alto
     this.x = 540 // posicion en el eje X (desde la derecha)
-    this.y = 0 // posicion en el eje Y (desde arriba)
+    this.y = yPosition // posicion en el eje Y (desde arriba)
+    // aleatorio entre -100 y 0
 
     // ajustar los valores en el DOM
     this.node.style.width = `${this.w}px`
@@ -20,8 +27,15 @@ class Obstacle {
     this.node.style.top = `${this.y}px`
 
     // velocidad del obstaculos
+    this.speed = 2;
   }
 
   // movimiento de los obstaculos
+  automaticMovement = () => {
+
+    this.x -= this.speed
+    this.node.style.left = `${this.x}px`
+
+  }
 
 }
